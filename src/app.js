de401d97c01de358c4d745417285e3721d3a07b2
Saeed Panahi <saeed.panahi@nova.local>
2022-11-20 18:20:53 +0330

    initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user')
+const itemRouter =require('./routers/item')
+// const cartRouter = require('./routers/cart')
+
+const app = express();
+app.use(express.json());
+
+app.use(userRouter)
+app.use(itemRouter)
+// app.use(cartRouter)
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    // Bash Color Flag: \x1B[35m => makes the command color 'magenta'
+    console.log(`\x1B[35mServer listening on port: ${port}`);
+})
---
+++ src/routers/user.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const Auth = require('../middleware/auth');
+const User = require('../models/user');
+
+const router = new express.Router();
+
+// signup
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// login
+router.post('/user/login', async (req, res) => {
+    try {
+        const user = User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.status(200).send({ user, token });
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// logout
+router.post('/user/logout', Auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+});
+
+// logout all
+router.post('/users/logoutAll', Auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+});
+
+module.exports = router;
