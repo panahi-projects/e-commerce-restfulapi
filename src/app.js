de3296bd5648568d5f6379d9c724d9111fc5ff18
Saeed Panahi <saeed.panahi@nova.local>
2022-11-23 22:49:00 +0330

    add cart router to app.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/user')
 const itemRouter =require('./routers/item')
-// const cartRouter = require('./routers/cart')
+const cartRouter = require('./routers/cart')
 
 const app = express();
 app.use(express.json());
 
 app.use(userRouter)
 app.use(itemRouter)
-// app.use(cartRouter)
+app.use(cartRouter)
 
 const port = process.env.PORT;
 
